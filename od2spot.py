3942ca4bed9d3b450f64295cc49e53c214306195
Mark Prager <33463586+maprager@users.noreply.github.com>
2020-02-20 17:15:14 +0200

    updated for tenacity and python3

--- od2spot.py
+++ od2spot.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
-
 #### Script to turn and on demand instance into a spot - any attached volumes will be moved
-#### Version 0.1 18/02/2020 Mark Prager
+#### Version 0.1 18/02/2020 Mark Prager - Initial version
+#### Version 0.2 20/20/2020 Mark Prager - tested on Python 3.6, and added tenacity.
+####
+#### Details:
 #### Tested on Python 2.7
-#### Tested only on Linux (Centos) Servers
-#### Limits - does not handle EIPs
-####          Makes some assumptions about the instances - e.g. root volume on /dev/sda1
+#### Tested on Python 3.6
+#### Converted only Linux (Centos) Servers
+####
+#### Limits:
+#### - does not handle EIPs
+#### - 5 minutes waiting period of AWS operations
+####
+#### Usage: od2spot.py -i <instance-id>
+####
+#### Credits:
+####     Mike Miller for Guidance
+############################################################################################
 
 ### Imports
 import boto3
@@ -14,13 +25,16 @@ import time
 import sys,re
 import logging
 import base64
+import argparse
 from datetime import datetime
+from tenacity import retry, wait_random_exponential, stop_after_delay
+from platform import python_version
 
 #### Simple function to print out the usage.
 def usage(text):
     if text:
         logging.critical(text)
-    logging.critical("Usage: {0} instance-id".format(sys.argv[0]))
+    logging.critical("Usage: {0} -i instance-id".format(sys.argv[0]))
 
 #### Simple function to pull out the instance name from the tags
 def get_instance_name(tags):
@@ -30,51 +44,66 @@ def get_instance_name(tags):
     return instancename
 
 #### Function to wait for a client image to be created.
+@retry(wait=wait_random_exponential(multiplier=1, max=10), stop=stop_after_delay(300))
 def wait_for_image_to_be_available(client,image_id):
-    try:
-        available = 0
-        while available == 0:
-            image = client.describe_images(ImageIds=[image_id])
-            if image['Images'][0]['State'] == 'available':
-                available = 1
-            else:
-                logging.info( "Image not created yet.. Gonna sleep for 10 seconds")
-                time.sleep(10)
-        if available == 1:
-            logging.info( "Image is now available for use.")
-            return True
-    except Exception, e:
-        logging.info( e)
+    image = client.describe_images(ImageIds=[image_id])
+    imageState = image['Images'][0]['State']
+    logging.info ( "waiting for image to be ready ....[%s]" % image_id)
+    assert imageState == "available"
+
+#### Function to wait for a spot instance to be created
+@retry(wait=wait_random_exponential(multiplier=1, max=10), stop=stop_after_delay(300))
+def wait_for_spot_request_state(client,state,sir):
+    check=client.describe_spot_instance_requests(SpotInstanceRequestIds=[sir])
+    logging.info ( "waiting for spot to reach state: %s"  % state )
+    assert check['SpotInstanceRequests'][0]['Status']['Code'] == state
+    return check['SpotInstanceRequests'][0]['InstanceId']
+
+#### Function to wait for an instance to get into a particular state.
+@retry(wait=wait_random_exponential(multiplier=1, max=10), stop=stop_after_delay(300))
+def wait_for_instance_state(client,instanceid, state):
+    response=ec2.describe_instances(InstanceIds=[instanceid])
+    instanceState=(response['Reservations'][0]['Instances'][0]['State']['Name'])
+    logging.info ( "waiting for instance to reach state: %s" % state )
+    assert instanceState == state
 
 ### Main
+
+### Read Command Line arguments
+parser = argparse.ArgumentParser(description='Script to convert AWS OnDemand Instances to Persistant Stoppable Spots')
+parser.add_argument('-i', '--instance-id', required=True, help='The instance Id to convert')
+args = vars(parser.parse_args())
+iid = args["instance_id"]
 
 ### Set up logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logging.basicConfig(format='%(levelname)s %(asctime)s %(message)s')
 
-### Read in the instance Id
-iid=sys.argv[1]
+pythonVersion = python_version()
 
 ### Print out Boto Version in case of support issues
-logging.info( "Boto Version: " + boto3.__version__)
-logging.info ("Attempting to migrate instance: " + iid )
+logging.info( "Boto Version: %s" % boto3.__version__)
+logging.info( "Running Python Version: %s" % pythonVersion )
+logging.info ("Attempting to migrate instance: %s" % iid )
+
 
 ### Get Current Date and Time.
 now = datetime.now()
-dt_string = now. strftime("%d%m%Y-%H%M%S")
+dt_string = now.strftime("%d%m%Y-%H%M%S")
 
 ec2= boto3.client("ec2")
 ## Gather facts including instance name
 try:
-	response=ec2.describe_instances(InstanceIds=[iid])
+        response=ec2.describe_instances(InstanceIds=[iid])
 except:
-	usage("Instance Id not valid: " + iid)
-        sys.exit()
+        usage("Instance Id not valid: " + iid)
+        sys.exit(1)
 
 VpcId=(response['Reservations'][0]['Instances'][0]['VpcId'])
 SubnetId=(response['Reservations'][0]['Instances'][0]['SubnetId'])
 Tags=(response['Reservations'][0]['Instances'][0]['Tags'])
+RootDeviceName=(response['Reservations'][0]['Instances'][0]['RootDeviceName'])
 KeyName=(response['Reservations'][0]['Instances'][0]['KeyName'])
 BlockDeviceMappings=(response['Reservations'][0]['Instances'][0]['BlockDeviceMappings'])
 InstanceType=(response['Reservations'][0]['Instances'][0]['InstanceType'])
@@ -85,34 +114,33 @@ InstanceName=get_instance_name(Tags)
 #Discover which is the root disk, and which are any 'extra' mounted disks
 extraDisks=[]
 for i in BlockDeviceMappings:
- if i['DeviceName'] == '/dev/sda1' :
+ if i['DeviceName'] == RootDeviceName :
      # Save Root Device Information
-     ## Get VolumeInformation
      rootVolume= i['Ebs']['VolumeId']
      volume=ec2.describe_volumes(VolumeIds=[rootVolume])
      rootIops=volume['Volumes'][0]['Iops']
      rootVolumeSize=volume['Volumes'][0]['Size']
      rootVolumeType=volume['Volumes'][0]['VolumeType']
- if i['DeviceName'] != '/dev/sda1' :
+ if i['DeviceName'] != RootDeviceName:
      extraDisks.append({'DeviceName': i['DeviceName'], 'VolumeId': i['Ebs']['VolumeId']})
 
-logging.info( extraDisks)
+# logging.info( extraDisks)
 
 ## Prepare Root Block Device
-## user for creating image - only do of root device - no need to copy all the volumes
+## user for creating image - only do the root device - no need to copy all the volumes
 ## will be used later also.
 rootBlockDevice=[]
-vol={'DeviceName': '/dev/sda1', 'Ebs': {'VolumeSize': rootVolumeSize, 'VolumeType': rootVolumeType}}
+vol={'DeviceName': RootDeviceName, 'Ebs': {'VolumeSize': rootVolumeSize, 'VolumeType': rootVolumeType}}
 ## Don't create any extra devices - they will be moved
 rootBlockDevice.append(dict(vol))
 for disk in extraDisks:
     vol={'DeviceName': disk['DeviceName'], 'NoDevice': ''}
     rootBlockDevice.append(dict(vol))
-logging.info(rootBlockDevice)
+# logging.info(rootBlockDevice)
 
 ### Create an image copy of the instance ( for cloning )
 response=ec2.create_image(BlockDeviceMappings=rootBlockDevice,InstanceId=iid, Name=InstanceName+"_clone-"+dt_string, NoReboot=True)
-logging.info( response['ImageId'])
+logging.info( "Image to be created: %s [%s]" % (response['ImageId'],InstanceName+"_clone-"+dt_string))
 ImageId=response['ImageId']
 wait_for_image_to_be_available(ec2,ImageId)
 
@@ -129,94 +157,63 @@ iface={'DeviceIndex': 0, 'DeleteOnTermination': True, 'SubnetId': SubnetId , 'Gr
 NetworkInterfaces.append(dict(iface))
 
 ### Here place the userdata to preserve the name of the instance, and change IPs etc....
+### If you have specifics for your server - you could place them here.
+### Alternatively, take this out, and readin the user data from the command line....
+### Maybe in a later version ... :)
 hostnameCmd="#!/bin/bash " + "\n"
 hostnameCmd= hostnameCmd + "sed -i '/^HOSTNAME=/ c \HOSTNAME=" + InstanceName +"' /etc/sysconfig/network\n"
 hostnameCmd= hostnameCmd + "ip=`curl -sf http://169.254.169.254/latest/meta-data/local-ipv4`\n"
 hostnameCmd= hostnameCmd + 'echo "${ip} ' + InstanceName +' localhost" > /etc/hosts\n'
 hostnameCmd= hostnameCmd + "echo '127.0.0.1 localhost.localdomain localhost4 localhost4.localdomain4' >> /etc/hosts\n"
-userdata=str(base64.b64encode(hostnameCmd))
+### Different commands in python 2 and 3
+if  sys.version_info < (3, 6 ) :
+	userdata=str(base64.b64encode(hostnameCmd))
+else:
+	userdata=base64.b64encode(hostnameCmd.encode()).decode("ascii")
 
 ## Prepare LaunchSpecification
 LaunchSpecification={'BlockDeviceMappings': rootBlockDevice,  'ImageId': ImageId, 'KeyName': KeyName, 'InstanceType': InstanceType, 'NetworkInterfaces': NetworkInterfaces , 'UserData': userdata}
-logging.info( LaunchSpecification)
+#logging.info( LaunchSpecification)
 
 ## Request Spot !
 spotRequest=ec2.request_spot_instances(Type='persistent', InstanceInterruptionBehavior='stop', LaunchSpecification=LaunchSpecification)
-logging.info( spotRequest)
-
 # Check Status of Spot request
 sir=spotRequest['SpotInstanceRequests'][0]['SpotInstanceRequestId']
-logging.info(sir)
-
-### Give AWS some time ....
-time.sleep(5)
-
-# wait till fully launched and running
-pending=0
-while pending == 0:
-    check=ec2.describe_spot_instance_requests(SpotInstanceRequestIds=[sir])
-    status=check['SpotInstanceRequests'][0]['Status']['Code']
-    if status == 'fulfilled' :
-         newInstanceId=check['SpotInstanceRequests'][0]['InstanceId']
-         pending=1
-    else:
-         time.sleep(1)
-         logging.info ("Spot instance is still pending...")
-
-logging.info( "New Instance Created: Stoppable Spot: " + newInstanceId)
+# logging.info(sir)
+newInstanceId=wait_for_spot_request_state(ec2,"fulfilled",sir)
+logging.info( "New Instance Created: Stoppable Spot: %s" % newInstanceId)
 
 ## Tag Instance
 response=ec2.create_tags(Resources=[newInstanceId], Tags=Tags)
 
 ## Wait for instance to fully come up:
-InstanceState="notup"
-while InstanceState != "running":
-    response=ec2.describe_instances(InstanceIds=[newInstanceId])
-    InstanceState=(response['Reservations'][0]['Instances'][0]['State']['Name'])
-    time.sleep(5)
-    logging.info ("Waiting for spot instance to be fully running....")
+wait_for_instance_state(ec2,newInstanceId,"running")
 
 ### Shutdown old Instance
-response=ec2.stop_instances(InstanceIds=[iid])
-InstanceState="running"
-while InstanceState != "stopped":
-    response=ec2.describe_instances(InstanceIds=[iid])
-    InstanceState=(response['Reservations'][0]['Instances'][0]['State']['Name'])
-    time.sleep(5)
-    logging.info ("Waiting for old OD instance to be fully stopped....")
+ec2.stop_instances(InstanceIds=[iid])
+wait_for_instance_state(ec2, iid, "stopped")
 
 ###  Detach old Volumes
 for vol in extraDisks:
-	response=ec2.detach_volume(InstanceId=iid, VolumeId=vol['VolumeId'] )
-        logging.info("Detaching Volumes: "+ vol['VolumeId'])
+        response=ec2.detach_volume(InstanceId=iid, VolumeId=vol['VolumeId'] )
+        logging.info("Detaching Volumes: %s " % vol['VolumeId'])
         time.sleep(1)
 
 ### Attach old volumes to new instance
 for vol in extraDisks:
-	response=ec2.attach_volume(InstanceId=newInstanceId, Device=vol['DeviceName'] ,VolumeId=vol['VolumeId'] )
-        logging.info("Attach Volumes: "+ vol['VolumeId'])
+        response=ec2.attach_volume(InstanceId=newInstanceId, Device=vol['DeviceName'] ,VolumeId=vol['VolumeId'] )
+        logging.info("Attach Volumes: %s " %  vol['VolumeId'])
         time.sleep(1)
 
+### Let AWS Rest ...
 time.sleep(5)
 
 ### Reboot instance
-response=ec2.stop_instances(InstanceIds=[newInstanceId])
-InstanceState="running"
-while InstanceState != "stopped":
-    response=ec2.describe_instances(InstanceIds=[newInstanceId])
-    InstanceState=(response['Reservations'][0]['Instances'][0]['State']['Name'])
-    time.sleep(5)
-    logging.info ("Waiting for new SPOT instance to be fully stopped....")
+ec2.stop_instances(InstanceIds=[newInstanceId])
+wait_for_instance_state(ec2,newInstanceId,"stopped")
+## Sleep 90 seconds - AWS needs time to adjust its spots status - not sure how to check this
+time.sleep(90)
+ec2.start_instances(InstanceIds=[newInstanceId])
+wait_for_instance_state(ec2,newInstanceId,"started")
 
-## Sleep 60 seconds - AWS needs time to adjust its spots status
-time.sleep(60)
-
-response=ec2.start_instances(InstanceIds=[newInstanceId])
-InstanceState="stopped"
-while InstanceState != "running":
-    response=ec2.describe_instances(InstanceIds=[newInstanceId])
-    InstanceState=(response['Reservations'][0]['Instances'][0]['State']['Name'])
-    time.sleep(5)
-    logging.info ("Waiting for new SPOT instance to be fully started....")
-
-logging.info ("Done")
+logging.info ("Conversion Done")
