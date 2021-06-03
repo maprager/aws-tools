50de8d39378d1ed69e60886c9e6b1926642b9b4e
Mark Prager <33463586+maprager@users.noreply.github.com>
2021-06-03 15:27:54 +0300

    Update show_vcpu_and_limit.sh

--- show_vcpu_and_limit.sh
+++ show_vcpu_and_limit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 region=$1
-vcpuLimit=$(aws service-quotas get-service-quota --region $region --service-code ec2 --quota-code L-1216C47A --query 'Quota.Value')
-vcpuInUse=$(aws ec2 describe-instances --region $region--filters "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].{"InstanceType": InstanceType,"CpuOptions": CpuOptions}' | grep -e "CoreCount" -e "ThreadsPerCore" | awk '{print $2}' | tr ',' ' ' | paste - - -d\* | perl -ne 'print eval $_,"\n"' |  perl -nle '$sum += $_ } END { print $sum')
 
-echo "You have $vcpuInUse in use out of the $vcpuLimit" 
+vcpuLimit=$(aws service-quotas get-service-quota --region $region --service-code ec2 --quota-code L-1216C47A --query 'Quota.Value')
+vcpuInUse=$(aws ec2 describe-instances --region $region --filters "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].{"InstanceType": InstanceType,"CpuOptions": CpuOptions}' | grep -e "CoreCount" -e "ThreadsPerCore" | awk '{print $2}' | tr ',' ' ' | paste - - -d\* | perl -ne 'print eval $_,"\n"' |  perl -nle '$sum += $_ } END { print $sum' )
+
+echo "You have $vcpuInUse vCPUSs in use out of the Limit: $vcpuLimit in region $region"
